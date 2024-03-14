cefa770ca7423f0ffc24d6ca9cb29629ac699751
Rayival Dede Saputra <113332359+Rayival@users.noreply.github.com>
2024-03-14 14:25:37 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="row p-3 bg-primary text-white">
+    <div class="col-lg-1">
+      <img src="~/assets/LOGO-SMK4.png" alt="logo SMKN 4">
+    </div>
+
+    <div class="col-lg-11">
+      <h1>Perpus Digital</h1>
+      <address>
+        SMKN 4 Tasikmalaya
+        JL.Depok, Sukamenak, Purbaratu
+      </address>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.bg-primary {
+  background-color: #1F75A6!important;
+  box-shadow: 1px 1px 10px #000000;
+}
+
+img {
+  width: 90px;
+}
+</style>
