b85e0820cfcf1ec1fcb386840900376128f3c6db
Rayival Dede Saputra <113332359+Rayival@users.noreply.github.com>
2024-05-03 09:13:38 +0700

    Update index.vue

--- pages/buku/index.vue
+++ pages/buku/index.vue
@@ -3,23 +3,23 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-lg-12">
-          <h2 class="text-center my-4">BUKU</h2>
-          <div class="my-3">
+          <!-- <h2 class="text-center my-4">BUKU</h2> -->
+          <div class="my-3 mt-5">
             <input type="search" class="form-control rounded-5" placeholder="Mau baca apa hari ini?">
           </div>
-          <div class="col-2 text-end " style="color: white;">
-                    <p class="m-0 pt-2">Kategori :</p>
-                </div>
-                <div class="col-2 ps-0">
-                    <select name="kategori" id="kategori" class="form-control form-control-sm rounded-5 form-select">
-                        <option value="" disabled selected>  </option>
-                        <option value="">Novel</option>
-                        <option value="">Pengetahuan Umum</option>
-                        <option value="">Bahasa</option>
-                        <option value="">Pengetahuan</option>
-                        <option value="">Cerita</option>
-                        <option value="">Pendidikan</option>
-                    </select>
+          <div class="row text-end " style="color: white;">
+            <p class="col-10 m-0 pt-2">Kategori :</p>
+            <div class="col-2 ps-0">
+              <select name="kategori" id="kategori" class="form-control form-control-sm rounded-5 form-select">
+                <option value="" disabled selected>  </option>
+                <option value="">Novel</option>
+                <option value="">Pengetahuan Umum</option>
+                <option value="">Bahasa</option>
+                <option value="">Pengetahuan</option>
+                <option value="">Cerita</option>
+                <option value="">Pendidikan</option>
+              </select>
+          </div>
           </div>
           <div class="my-3 text-muted">menampilkan 3 dari 3</div>
           <div class="row">
