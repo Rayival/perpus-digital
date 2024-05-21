0f9c765b6b2d1fbd12830bccef751647bc1811f3
Rayival Dede Saputra <113332359+Rayival@users.noreply.github.com>
2024-05-21 07:37:49 +0700

    Update index.vue

--- pages/pengunjung/index.vue
+++ pages/pengunjung/index.vue
@@ -5,38 +5,60 @@
       <div class="col-lg-12">
         <h2 class="text-center my-4">RIWAYAT KUNJUNGAN</h2>
         <div class="my-3">
+          <input type="search" class="form-control form-control-lg rounded-5" placeholder="Filter...">
         </div>
         <div class="my-3 text-muted">menampilkan 1 dari </div>
         <table class="table table-bordered border-dark text-white ">
           <thead>
-            <tr>
-              <td>#</td>
-              <td>NAMA</td>
-              <td>KATEGORI</td>
-              <td>WAKTU</td>
-              <td>KEPERLUAN</td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1.</td>
-              <td>ival</td>
-              <td>Siswa</td>
-              <td>24 Februari 2024, 23.31.00</td>
-              <td>Baca</td>
-            </tr>
-          </tbody>
+              <tr class="text-center">
+                <td>ID</td>
+                <td>NAMA</td>
+                <td>KEANGGOTAAN</td>
+                <td>KELAS</td>
+                <td>KEPERLUAN</td>
+                <td>TANGGAL</td>
+                <td>JAM</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(visitors,i) in visitors" :key="i" class="text-center">
+                <td>{{ i+1 }}.</td>
+                <td>{{ visitors.nama }}</td>
+                <td>{{ visitors.keanggotaan.nama }}</td>
+                <td>{{ visitors.tingkat}}-{{ visitors.jurusan }}{{ visitors.kelas }}</td>
+                <td>{{ visitors.keperluan.nama }}</td>
+                <td>{{ visitors.tanggal }}</td>
+                <td>{{ visitors.jam.split(".")[0] }} </td>
+              </tr>
+            </tbody>
         </table>
+        </div>
+        </div>
       </div>
     </div>
-  </div>
+  
   <!-- <button type="submit" class="btn btn-dark btn-lg rounded-5 px-5">Mencari Buku</button> -->
   <nuxt-link to="/buku" class="btn btn-dark btn-lg rounded-5 px-5 mx-2">Mencari Buku</nuxt-link>
 
   <!-- <button type="submit" class="btn btn-dark btn-lg rounded-5 px-5">Selesai</button> -->
   <nuxt-link to="/" class="btn btn-dark btn-lg rounded-5 px-5">Selesai</nuxt-link>
-</div>
 </template>
+
+<script setup>
+const supabase= useSupabaseClient()
+
+const visitors = ref([])
+const getPengunjung =async () => {
+  const { data, error } = await supabase.from('pengunjung').select(`*, keanggotaan(*), keperluan(*)`)
+  if(data) visitors.value = data
+}
+onMounted(() =>{
+  getPengunjung()
+})
+
+
+
+</script>
 
 <style scoped>
 .content {
